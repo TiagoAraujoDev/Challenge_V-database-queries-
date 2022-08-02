cc332d9ea439a906ef2722822fb3e7bc84c5e8a2
TiagoAraujoDev <102497603+TiagoAraujoDev@users.noreply.github.com>
2022-08-02 06:47:47 -0300

    finished

---
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -0,0 +1,34 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { User } from '../../../users/entities/User';
+import { Game } from '../../entities/Game';
+
+import { IGamesRepository } from '../IGamesRepository';
+
+export class GamesRepository implements IGamesRepository {
+  private repository: Repository<Game>;
+
+  constructor() {
+    this.repository = getRepository(Game);
+  }
+
+  async findByTitleContaining(param: string): Promise<Game[]> {
+    return await this.repository
+      .createQueryBuilder("games")
+      .where("title ILIKE :param", { param: `%${param}%`})
+      .getMany()
+  }
+
+  async countAllGames(): Promise<[{ count: string }]> {
+    // Complete usando raw query
+    return await this.repository.query("SELECT COUNT(id) FROM games");
+  }
+
+  async findUsersByGameId(id: string): Promise<User[]> {
+    return await this.repository
+      .createQueryBuilder("games")
+      .relation(Game, "users")
+      .of(id)
+      .loadMany()
+  }
+}
